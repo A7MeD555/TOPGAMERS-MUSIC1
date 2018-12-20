a16b8d53f065b69cab5906a8cfef259f738554c7
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-12-20 18:09:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,20 +35,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
  
- client.on('message', msg => {
 
-    if (msg.content == '1join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client.on('ready', () => { //code bot not leave room voice //Bot Is Online
-    client.channels.get("522898636791742464").join(); //by : Toxic Codes
-    });
  
 var prefix = "1" 
 client.on('message', async msg => {
